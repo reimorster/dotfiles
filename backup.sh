74425386bad2a147122f9fa56d0200fd6b637865
reimorster <reimorster@gmail.com>
2023-03-18 02:22:49 -0300

    creating some functions , still not functional

--- backup.sh
+++ backup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+VERSION="0.1"
+
 
 FILES_TO_COPY=(.zshrc .bashrc)
 
@@ -7,3 +9,24 @@ for f in ${FILES_TO_COPY[@]}; do
     echo Copying $f
     [[ -e ~/$f ]] && cp  ~/$f .
 done
+
+
+#TODO:
+#
+# rename the script to dotfiles
+#
+# parse arguments -b for backup and -r for restore, -m for optional message for commit when backing up
+#
+
+function usage() {
+    script=$(basename $0)
+    echo $script - version $VERSION
+}
+
+function backup() {
+    echo not implemented
+}
+
+function commit() {
+    git add . && git commit -m "$1"
+}
